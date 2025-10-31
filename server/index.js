42d7cb9910fcb39e89168ecc976f8f9e018c822f
Sambit Sahoo <sambitsahoo191@gmail.com>
2025-10-31 08:44:23 +0530

    mongo

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: "http://localhost:5173",  
     credentials: true,
   })
 );
